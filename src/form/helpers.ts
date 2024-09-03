315c2c9df81d7327a4bfc3f6cd60a788fcefc437
Joey Bishop <54321112+NeoBaneling@users.noreply.github.com>
2024-09-02 20:24:31 -0400

    Resolve calculations (#6)

--- src/form/helpers.ts
+++ src/form/helpers.ts
@@ -1,10 +1,46 @@
+import { FormData } from './initialValues';
+
 export const getDayText = (day: number) =>
   [
-    'Sunday',
     'Monday',
     'Tuesday',
     'Wednesday',
     'Thursday',
     'Friday',
     'Saturday',
+    'Sunday',
   ][day];
+
+const streakScore = (streak: number) => {
+  if (streak === 7) return 7;
+  if (streak >= 5) return 5;
+  return 0;
+};
+
+export const calculateScore = (formData: FormData): number => {
+  let numClosedRings = 0;
+  let numSteps = 0;
+
+  let sum = formData.reduce(
+    (
+      score,
+      { milesWalked, otherExercise, closedRings, closedExerciseRing, steps }
+    ) => {
+      score += Math.floor(milesWalked);
+
+      if (otherExercise) score += 2;
+      if (closedExerciseRing) score += 1;
+
+      if (closedRings) numClosedRings++;
+      if (steps) numSteps++;
+
+      return score;
+    },
+    0
+  );
+
+  sum += streakScore(numClosedRings);
+  sum += streakScore(numSteps);
+
+  return sum;
+};
